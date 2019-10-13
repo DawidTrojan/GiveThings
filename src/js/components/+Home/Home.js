b3dcf7261f2f3ce421c67b66d0163efb6f4bc760
Dawid Trojan <daw.trojan@gmail.com>
2019-10-13 11:12:13 +0200

    added section three columns

--- src/js/components/+Home/Home.js
+++ src/js/components/+Home/Home.js
@@ -1,6 +1,12 @@
 import React from "react";
 import HomeHeader from "./+HomeHeader";
+import HomeThreeColumns from "./+HomeThreeColumns";
 
-const Home = () => <HomeHeader />;
+const Home = () => (
+  <div className="layout_container">
+    <HomeHeader />
+    <HomeThreeColumns />
+  </div>
+);
 
 export default Home;
