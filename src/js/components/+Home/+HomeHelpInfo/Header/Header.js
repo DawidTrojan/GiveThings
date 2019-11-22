533793ad2dfcdb09da273475852ca644c7732d24
Dawtro <51854427+Dawtro@users.noreply.github.com>
2019-11-22 14:16:41 +0100

    added part of responsive styles

--- src/js/components/+Home/+HomeHelpInfo/Header/Header.js
+++ src/js/components/+Home/+HomeHelpInfo/Header/Header.js
@@ -26,42 +26,48 @@ const Header = () => {
 
   return (
     <div className="help_info__container">
-      <div className="help_info__header">
-        <h4>Komu pomagamy?</h4>
-        <div className="help_info__decoration"></div>
-        <div className="help_info__buttons">
-          {isActiveFundation ? (
-            <span id="fundations" className="active" onClick={handleOnClick}>
-              Fundacjom
-            </span>
-          ) : (
-            <span id="fundations" onClick={handleOnClick}>
-              Fundacjom
-            </span>
-          )}
-          {isActiveOrganization ? (
-            <span id="organizations" className="active" onClick={handleOnClick}>
-              Organizajom pozarządowym
-            </span>
-          ) : (
-            <span id="organizations" onClick={handleOnClick}>
-              Organizajom pozarządowym
-            </span>
-          )}
-          {isActiveLocal ? (
-            <span id="local" className="active" onClick={handleOnClick}>
-              Lokalnym zbiórkom
-            </span>
-          ) : (
-            <span id="local" onClick={handleOnClick}>
-              Lokalnym zbiórkom
-            </span>
-          )}
+      <div className="wrapper">
+        <div className="help_info__header">
+          <h4>Komu pomagamy?</h4>
+          <div className="help_info__decoration"></div>
+          <div className="help_info__buttons">
+            {isActiveFundation ? (
+              <span id="fundations" className="active" onClick={handleOnClick}>
+                Fundacjom
+              </span>
+            ) : (
+              <span id="fundations" onClick={handleOnClick}>
+                Fundacjom
+              </span>
+            )}
+            {isActiveOrganization ? (
+              <span
+                id="organizations"
+                className="active"
+                onClick={handleOnClick}
+              >
+                Organizajom pozarządowym
+              </span>
+            ) : (
+              <span id="organizations" onClick={handleOnClick}>
+                Organizajom pozarządowym
+              </span>
+            )}
+            {isActiveLocal ? (
+              <span id="local" className="active" onClick={handleOnClick}>
+                Lokalnym zbiórkom
+              </span>
+            ) : (
+              <span id="local" onClick={handleOnClick}>
+                Lokalnym zbiórkom
+              </span>
+            )}
+          </div>
         </div>
+        {isActiveFundation ? <Fundations /> : null}
+        {isActiveOrganization ? <Organizations /> : null}
+        {isActiveLocal ? <LocalCollections /> : null}
       </div>
-      {isActiveFundation ? <Fundations /> : null}
-      {isActiveOrganization ? <Organizations /> : null}
-      {isActiveLocal ? <LocalCollections /> : null}
     </div>
   );
 };
